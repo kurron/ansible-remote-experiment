952704c9e6f897ec6fd01ac136700144a734a238
Ron Kurr <kurr@kurron.org>
2015-03-28 20:47:02 -0400

    Got my first playbook working

--- run-ansible.sh
+++ run-ansible.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ansible docker --verbose  --inventory-file=hosts --args "/bin/echo hello"
+#ansible docker --verbose  --inventory-file=hosts --args "/bin/echo hello"
+ansible-playbook  -vvvv  --inventory-file=hosts playbook.yml
